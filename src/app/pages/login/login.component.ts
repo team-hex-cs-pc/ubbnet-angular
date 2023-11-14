f117f6d13121dd98cc4b49c7012b9a5f022b7eef
David Alexandru <68737671+davidalexandru1370@users.noreply.github.com>
2023-11-14 23:15:05 +0200

    Feat/login (#4)
    * added: login page and functionality
    
    * added: error handling for login
    
    * added: user interface and fix pr comments

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
+  errorAfterLogin: string = '';
+
+  async login(username: string, password: string) {
+    try {
+      const res = await this.userService.login(username, password);
+      if (res) {
+        this.errorAfterLogin = '';
+        localStorage.setItem('token', res.token);
+        await this.userService.getUserInformation();
+        this.userService.user$.subscribe((user) => {
+          console.log(user);
+        });
+      } else {
+        this.errorAfterLogin = 'Invalid username or password';
+      }
+    } catch (err: unknown) {
+      this.errorAfterLogin = 'Invalid username or password';
+    }
+  }
+}
