e6c52dcfb98e1db9b9861e2d2c10d2dee2d5bb2f
David Alexandru <fanefaraon696@gmail.com>
2023-11-15 13:32:39 +0200

    added: cover image

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,8 +3,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
