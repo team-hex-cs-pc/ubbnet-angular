e0381501a2b1d2f5928c6c562ddd8a5de091771d
Laurentiu Pricop <laurcons@outlook.com>
2023-11-21 23:56:22 +0200

    refactor: delete gitignored file

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./environment.d.ts" />
-// local configuration for local development
-// will be replaced at build time
-export const environment: Environment = {
-  apiUrl: 'https://dev.api.teamhex.laurcons.ro/api',
-};
