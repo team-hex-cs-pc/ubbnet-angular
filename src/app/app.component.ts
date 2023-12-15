b6f91bda2b389e6884f6da8c77f0c117eaace87a
Irina Scortar <irinascortar@gmail.com>
2023-12-15 16:28:31 +0200

    - added profile menu entry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
-import { ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
 import {UserService} from "./services/user.service";
 
 @Component({
